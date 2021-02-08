d0dcec95924ee8c67e2c35cd3217c542f8aeb9dd
neel004 <neelp0004@gmail.com>
2021-02-09 00:41:28 +0530

    Init Commit

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -17,7 +17,7 @@ const auth = async (req,res,next)=>{
         next()
 
     }catch(e){
-        res.status(401).send({ error : "please Authenticate" })
+        res.status(401).send({ error : "please Authenticate",E:e })
     }
 }
 
