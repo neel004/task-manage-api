153819cf040375ab445d21959e984ff87fe2aa5f
neel004 <neelp0004@gmail.com>
2021-02-08 23:28:17 +0530

    Init Commit

--- src/routers/users.js
+++ src/routers/users.js
@@ -10,11 +10,11 @@ router.post('/users',async (req,res)=>{
     const user = new User(req.body)
     try{
         await user.save()
-        //sendWelcomeEmail(user.email,user.name)
+        sendWelcomeEmail(user.email,user.name)
         const token = await user.generateAuthToken()
         res.status(201).send({user,token})
     }catch(e){
-        res.status(400).send(e)
+        res.status(401).send(e)
     }
     // user.save().then(()=>{
     //     res.status(201).send(user)
